dd96f765c7d3a4d5cab18f8cf32d4c590ef8489a
durv_eshhh <durveshparab1111@gmail.com>
2026-04-04 10:51:58 +0530

    create password field for Users

--- app/schemas.py
+++ app/schemas.py
@@ -5,6 +5,20 @@ class UserCreate(BaseModel):
     name: str
     email: str
     role: str
+    password: str
+
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    email: str
+    role: str
+
+    class Config:
+        from_attributes = True
+
+class UserLogin(BaseModel):
+    email: str
+    password: str
 
 
 class TransactionCreate(BaseModel):
