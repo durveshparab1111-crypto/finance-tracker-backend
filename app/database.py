9f80cbb99c43984769410e2ce12cf6291521db67
durv_eshhh <durveshparab1111@gmail.com>
2026-04-04 11:13:33 +0530

    create env file

--- app/database.py
+++ app/database.py
@@ -1,8 +1,12 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:durvesh1107@localhost:5432/finance_tracker"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
